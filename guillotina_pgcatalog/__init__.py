a517cb708356776dd50aa9dd49b2fdd77f7c89df
Ramon Navarro Bosch <ramon.nb@gmail.com>
2018-11-13 19:09:21 +0100

    Loading utilities on __init__.py

--- guillotina_pgcatalog/__init__.py
+++ guillotina_pgcatalog/__init__.py
@@ -1,6 +1,13 @@
 from guillotina import configure
 
 app_settings = {
+    "load_utilities": {
+        "catalog": {
+            "provides": "guillotina_pgcatalog.utility.ICatalogUtility",  # noqa
+            "factory": "guillotina_pgcatalog.utility.PGSearchUtility",
+            "settings": {}
+        }
+    },
 }
 
 
